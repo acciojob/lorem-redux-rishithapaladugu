5821fb2ade9088b4439c0ac5b270ed6a8082f71c
rishithapaladugu <116055808+rishithapaladugu@users.noreply.github.com>
2025-10-21 11:39:49 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import store from './store';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+import {Provider} from 'react-redux'
+import store from './store'
 
-ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById('root')
-);
+
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById("root"));
