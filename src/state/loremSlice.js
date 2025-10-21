1f6f7ee6792c6f267373bc813140e45e1f96876b
rishithapaladugu <116055808+rishithapaladugu@users.noreply.github.com>
2025-10-21 11:56:36 +0530

    Update loremSlice.js

--- src/state/loremSlice.js
+++ src/state/loremSlice.js
@@ -30,11 +30,11 @@ const loremSlice = createSlice({
       .addCase(fetchLorem.pending, (state) => {
         state.loading = true;
         state.error = null;
-      })
+      });
       .addCase(fetchLorem.fulfilled, (state, action) => {
         state.loading = false;
         state.data = action.payload;
-      })
+      });
       .addCase(fetchLorem.rejected, (state, action) => {
         state.loading = false;
         state.error = action.error.message;
