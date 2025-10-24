f475029f7858c5fd1ea6e3ecfcce24a9b9484b8a
rishithapaladugu <116055808+rishithapaladugu@users.noreply.github.com>
2025-10-24 18:53:20 +0530

    Update actions.js

--- src/store.js
+++
@@ -1,7 +0,0 @@
-import {configureStore} from '@reduxjs/toolkit'
-import  loremReducer  from './state/loremSlice'
-export default configureStore({
-    reducer:{
-        lorem: loremReducer
-    }
-});
