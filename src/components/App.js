8be0d23f13a3c9c76279c8090629fbc649f7dcc0
rishithapaladugu <116055808+rishithapaladugu@users.noreply.github.com>
2025-10-30 11:04:19 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,35 +1,41 @@
-import React, { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { fetchLorem } from "./loremSlice";
 import "./../styles/App.css";
+import React, { useEffect, useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { fetchPosts } from "./LoremSlice";
 
 const App = () => {
   const dispatch = useDispatch();
-  const { data, loading } = useSelector((state) => state.lorem);
+  const { posts, loading } = useSelector((state) => state.lorem);
+  const [showIntro, setShowIntro] = useState(true);
 
   useEffect(() => {
-    dispatch(fetchLorem());
+    const timer = setTimeout(() => {
+      setShowIntro(false);
+      dispatch(fetchPosts());
+    }, 1000);
+    return () => clearTimeout(timer);
   }, [dispatch]);
 
   return (
-    <div className="body">
-      {loading && !data && (
-        <h4>Loading...</h4>
-      )}
+    <div className="app-container">
+      <h1>A short Naration of Lorem Ipsum</h1>
 
-      {!loading && data && (
-        <>
-          <h1>A short Naration of Lorem Ipsum</h1>
-          <h4>
-            Below Contains A title and Body gotten froma random API, Please take your time to Review
-          </h4>
-          <ul>
-            <li>
-              <span className="title">Title :{data.title}</span>
+      {showIntro ? (
+        <p>
+          Below Contains A title and Body gotten froma random API, Please take
+          your time to Review
+        </p>
+      ) : loading ? (
+        <h4>Loading...</h4>
+      ) : (
+        <ul>
+          {posts.map((post) => (
+            <li key={post.id}>
+              <h4 className="title">Title :{post.title}</h4>
+              <p className="body">Body :{post.body}</p>
             </li>
-            <li>Body: {data.body}</li>
-          </ul>
-        </>
+          ))}
+        </ul>
       )}
     </div>
   );
