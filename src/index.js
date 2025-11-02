5058fc1e25c4434c9856e2ae7c3c51aeaa5c2155
rishithapaladugu <116055808+rishithapaladugu@users.noreply.github.com>
2025-11-02 11:21:49 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import App from "./components/App";
 import { Provider } from "react-redux";
-import store from "./redux/store";
-import App from "./components/App.js";
+import store from "./components/store";
+import "regenerator-runtime/runtime";
 
 ReactDOM.render(
   <Provider store={store}>
