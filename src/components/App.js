6d43cc7406ec322f187dbb39b4774edfebcd7529
rishithapaladugu <116055808+rishithapaladugu@users.noreply.github.com>
2025-11-02 10:58:29 +0530

    Update App.js

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import DisplayData from "./DisplayData.js";
+function App() {
+  return (
+    <div className="App">
+      <DisplayData />
+    </div>
+  );
+}
+
+export default App;
