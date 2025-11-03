67ee8fdf9a403d723af93a5e02cfd0e06f184eb2
rishithapaladugu <116055808+rishithapaladugu@users.noreply.github.com>
2025-11-03 11:03:23 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,33 +1,21 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { fetchLorem } from "./actions";
+import { fetchLorem } from "../store/actions";
+import Loader from "./Loader";
+import LoremDisplay from "./LoremDisplay";
 
-function App() {
+const App = () => {
   const dispatch = useDispatch();
-  const { loading, title, body, posts, error } = useSelector((state) => state);
+  const { loading, data, error } = useSelector((state) => state);
 
   useEffect(() => {
     dispatch(fetchLorem());
   }, [dispatch]);
 
-  if (loading) {
-    // ✅ Cypress expects this exact tag
-    return <h4>Loading...</h4>;
-  }
+  if (loading) return <Loader />;
+  if (error) return <p>Error: {error}</p>;
 
-  if (error) {
-    return <p>Error: {error}</p>;
-  }
-
-  return (
-    <div>
-      <h1>{title}</h1>
-      <p>{body}</p>
-      <ul>
-        {posts && posts.map((p, i) => <li key={i}>{p}</li>)}
-      </ul>
-    </div>
-  );
-}
+  return <LoremDisplay data={data} />;
+};
 
 export default App;
